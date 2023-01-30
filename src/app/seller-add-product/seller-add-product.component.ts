c704e04c03059462c757c06235c16769797aef15
Hasul79 <hatuk2@mail.ru>
2023-01-31 00:13:37 +0400

    add product from input

--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-seller-add-product',
   templateUrl: './seller-add-product.component.html',
   styleUrls: ['./seller-add-product.component.css']
 })
-export class SellerAddProductComponent {
+export class SellerAddProductComponent implements OnInit{
 
+
+   constructor() { }
+
+   ngOnInit(): void {
+     
+   }
+
+   addProductData(data: object){
+     console.warn(data);
+     
+   }
 }
