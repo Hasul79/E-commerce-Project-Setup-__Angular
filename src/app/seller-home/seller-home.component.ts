4bbd4bdfed1a633add155f981051ba1abf9c8317
Hasul79 <hatuk2@mail.ru>
2023-02-01 23:52:06 +0400

    post/get

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-seller-home',
   templateUrl: './seller-home.component.html',
   styleUrls: ['./seller-home.component.css']
 })
-export class SellerHomeComponent {
+export class SellerHomeComponent implements OnInit{
 
+   constructor(private product: ProductService){ }
+
+    ngOnInit(): void {
+      this.product.productList().subscribe((result => {
+        console.warn(result);
+        
+      }))
+    }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -13,4 +13,8 @@ export class ProductService {
     return this.http.post('http://localhost:3000/products', data);
     
    }
+
+   productList(){
+    return this.http.get('http://localhost:3000/products');
+   }
 }
